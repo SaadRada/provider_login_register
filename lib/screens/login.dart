9da814c3f29bf7330d4c76f311dd878ae18e87ec
saadrada <saadrada742@gmail.com>
2022-11-29 10:40:50 +0100

    create folder structure, add login screen with simple text widget

---
+++ lib/screens/login.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          child: Text('Login Screen'),
+        ),
+      ),
+    );
+  }
+}
